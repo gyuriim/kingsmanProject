722b2280a1f347f73e50bc851afed0689479ff78
gyurim <124134421+gyuriim@users.noreply.github.com>
2023-02-05 19:43:34 +0900

    이성환

---
+++ js/collection.js
@@ -0,0 +1,18 @@
+$(".accordionHeader").on("click", function () {
+  $(".accordionContent").removeClass("active");
+});
+$("#ah1").on("click", function () {
+  $("#ac1").addClass("active");
+});
+$("#ah2").on("click", function () {
+  $("#ac2").addClass("active");
+});
+$("#ah3").on("click", function () {
+  $("#ac3").addClass("active");
+});
+$("#ah4").on("click", function () {
+  $("#ac4").addClass("active");
+});
+$("#ah5").on("click", function () {
+  $("#ac5").addClass("active");
+});
